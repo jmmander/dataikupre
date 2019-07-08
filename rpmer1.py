edb4efa31a284a342f895e443ed3fdf166d88376
Jacqueline <maaaander@gmail.com>
2019-07-08 16:31:12 -0400

    added more functions

--- rpmer1.py
+++ rpmer1.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 import rpm
+import os
+import subprocess
+import re
+import shlex
+replist = []
+
+
 
 
 class bcolors:
@@ -21,7 +28,7 @@ def bird():
 
 
 def intro():
-    return "************************************************\n          Dataiku pre-installation report\n************************************************\n\n   Making sure all your eggs are in the nest!         \n\n"
+    return "**************************************************\n          Dataiku pre-installation report\n**************************************************\n\n   Making sure all your eggs are in the nest!         \n\n"
 
 
 prereqs = ["hadoop-client", "hadoop-lzo", "spark-core", "spark-python", "spark-R", "spark-datanucleus", "hive",
@@ -58,44 +65,162 @@ def checkforstuff(prereqs, instpkg, numwc):
                 x += 1
                 if pkg == inpkg[0:len(pkg)]:
                     success.append(pkg)
-                    print(good(inpkg))
+                    print(good(inpkg, replist))
                 elif x / (len(wclist)) == len(instpkg):
                     if len(wclist) == numwc:
                         for wcpkg in wclist:
                             if wcpkg not in success:
                                 nomissing += 1
-                                print(fail(wcpkg, wclist))
+                                print(fail(wcpkg, wclist, replist))
                     else:
                         continue
         else:
             if req in instpkg:
-                print(good(req))
+                print(good(req, replist))
             else:
                 nomissing += 1
-                print(fail(req, wclist))
+                print(fail(req, wclist, replist))
     return nomissing
 
-def fail(req, wclist):
+def red(text):
+    return (bcolors.FAIL + text + bcolors.ENDC)
+
+def green(text):
+    return (bcolors.OKGREEN + text + bcolors.ENDC)
+
+def fail(req, wclist,replist):
     if req in wclist:
-        nokay = req + "* is not installed"
+        nokay = req + "* is missing from the nest"
+        replist.append(nokay)
     else:
-        nokay = req + " is not installed"
-    return (bcolors.FAIL + nokay + bcolors.ENDC)
+        nokay = req + " is missing from the nest"
+        replist.append(nokay)
+    return red(nokay)
 
 
-def good(req):
-    okay = req + (" is installed")
-    return (bcolors.OKGREEN + okay + bcolors.ENDC)
+def good(req,replist):
+    okay = req + (" is safe in the nest")
+    replist.append(okay)
+    return green(okay)
 
+def result(nomissing):
+    if nomissing == str(len(prereqs)):
+        return"Uh oh! Looks like you have no eggs in your nest :("
+    elif nomissing > 1:
+        return "Uh oh! Looks like you are missing " + str(nomissing) + " eggs! :("
+    elif nomissing == 1:
+        return "Uh oh! Looks like you are missing " + str(nomissing) + " egg! :("
+    else:
+        return "Look at you go! You have all your eggs and are ready to start hatching :)"
+
+
+
+
+#Running on a system with Security-Enhanced Linux (SELinux) in enforcing mode is not supported.
+def selinux():
+    process = subprocess.Popen(['getenforce'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if "Disabled" in stdout:
+        okay = "Nice work! Your nest does not have SELinux enabled"
+        replist.append(okay)
+        return green(okay)
+    else:
+        nokay = "Uh oh! Your nest has SELinux enabled"
+        replist.append(nokay)
+        return red(nokay)
+
+
+
+
+
+#A minimum of 16 GB of RAM is required. More RAM will be required if you intend to load large datasets in memory (for example in the Jupyter notebook component), or for accomodating more users.
+def ram():
+    with open('/proc/meminfo') as mem:
+        memory = mem.readline()
+        nmem = re.sub("[^0-9]", "", memory)
+        num = float(nmem)
+        gb = round((num/(1024*1024)),2)
+        okay = "Your nest is nice and roomy, you have " + str(gb) + "GB" + " of space."
+        nokay = "Uh oh! Your nest is a little small, you only have " + str(gb) + "GB" + " of space."
+        if gb < 16:
+            replist.append(nokay)
+            return(red(nokay))
+        else:
+            replist.append(okay)
+            return(green(okay))
+
+        mem.closed
+
+
+
+#The hard limit on the maximum number of open files for the Unix user account running DSS should be at least 65536 (ulimit -Hn). For very large DSS instances, larger values may be required.
+def openfiles():
+    process = subprocess.Popen(["ulimit -Hn"], shell=True, stdout=subprocess.PIPE)
+    stdout_val = process.communicate()
+    num = stdout_val[0]
+    lim = re.sub("[^0-9]", "", num)
+    hardlimit = int(lim)
+    if hardlimit >= 65536:
+        okay = "Nice work! Your nest can have " + str(hardlimit) + " files open."
+        replist.append(okay)
+        return green(okay)
+    else:
+        nokay = "Uh oh! Your nest can only have " + str(hardlimit) + " files open."
+        replist.append(nokay)
+        return red(nokay)
+
+
+
+#The hard limit on the maximum number of user processes for the Unix user account running DSS should be at least 65536 (ulimit -Hu). For very large DSS instances, larger values may be required.
+def userprocesses():
+    process = subprocess.Popen(["ulimit -Hu"], shell=True, stdout=subprocess.PIPE)
+    stdout_val = process.communicate()
+    num = stdout_val[0]
+    lim = re.sub("[^0-9]", "", num)
+    hardlimit = int(lim)
+    if hardlimit >= 65536:
+        okay = "Nice work! Your nest can have " + str(hardlimit) + " processes running."
+        replist.append(okay)
+        return green(okay)
+    else:
+        nokay = "Uh oh! Your nest can only have " + str(hardlimit) + " processes running."
+        replist.append(nokay)
+        return red(nokay)
+
+
+#The en_US.utf8 locale must be installed.
+def locale():
+    process = subprocess.Popen(['locale'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if "en_US.UTF-8" in stdout:
+        okay = "Nice work! You are using en_US.utf8"
+        replist.append(okay)
+        return green(okay)
+    else:
+        nokay = "Uh oh! You are not using en_US.utf8"
+        replist.append(nokay)
+        return red(nokay)
+
+
+def report(replist):
+    with open('dku-report.txt', 'w+') as report:
+        for item in replist:
+            report.write("%s\n" % item)
+        report.closed
+    #with open('dku-report.txt', 'r') as reading:
+     #   print(reading.read())
+      #  reading.closed
 
 print(bird())
 print(intro())
-checkforstuff(prereqs, makedict(), wccount(prereqs))
+num = checkforstuff(prereqs, makedict(), wccount(prereqs))
+print("\n\n"+result(num)+ "\n\n")
+print(selinux())
+print(ram())
+print(openfiles())
+print(userprocesses())
+print(locale())
+report(replist)
 
-def total(nomissing):
-    if nomissing == len(prereqs):
-        return"Uh oh! Looks like the python ate all the eggs in your nest :("
-    elif nomissing > 0:
-        return "Uh oh! Looks like the python ate " + nomissing + " eggs! :("
-    else:
-        return "Look at you go! You have all your eggs and are ready to start hatching :)"
+
+
